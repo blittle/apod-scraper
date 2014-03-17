9c57fc63b2925f90102e5de4d69591098c01b06e
Bret Little <bret.little@gmail.com>
2014-03-17 17:06:42 -0600

    Add a medium thumnail

--- src/utils/ImageWriter.js
+++ src/utils/ImageWriter.js
@@ -9,16 +9,22 @@ var THUM_W = 300,
 
 mkdirp(IMAGES_BASE + '/thum');
 mkdirp(IMAGES_BASE + '/full');
+mkdirp(IMAGES_BASE + '/med');
 
-function createThumb(source, dest, callback) {
+function createThumb(source, thumPath, medPath, callback) {
 	gm(source)
-		.thumb(THUM_W, THUM_H, dest, QUALITY, callback);
+		.thumb(THUM_W, THUM_H, thumPath, QUALITY, function(err) {
+			if(err) return callback(err);
+			gm(source)
+				.thumb(THUM_W * 2, THUM_H * 2, medPath, QUALITY, callback);
+		});
 }
 
 exports.saveImage = function(path, callback) {
 	var fileName = path.substring(path.lastIndexOf('/') + 1);
 	var fullPath = IMAGES_BASE + '/full/' + fileName;
 	var thumPath = IMAGES_BASE + '/thum/' + fileName;
+	var medPath = IMAGES_BASE + '/med/' + fileName;
 
 	http.get('http://apod.nasa.gov/apod/' + path, fullPath, function(err) {
 		if(err) {
@@ -27,7 +33,7 @@ exports.saveImage = function(path, callback) {
 		}
 		else {
 			console.log('Image downloaded: ' + fullPath);
-			createThumb(fullPath, thumPath, function(err) {
+			createThumb(fullPath, thumPath, medPath, function(err) {
 				if(err) {
 					console.log(err);
 					callback(err);
@@ -36,7 +42,8 @@ exports.saveImage = function(path, callback) {
 					console.log('Thumbnail crated: ' + thumPath);
 					callback(null, {
 						fullPath: fullPath,
-						thumPath: thumPath
+						thumPath: thumPath,
+						medPath: medPath
 					});
 				}
 			})
